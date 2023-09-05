2683a380dfcfb97f241bcd272fea299ff4e3843e
Krislucas <krislucasthomas@gmail.com>
2023-09-05 18:08:46 +0530

    Added same company vehicles component

---
+++ src/components/ViewVehicle/vehicle-3vp/thirdvp.jsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
+import "./thirdvp.css";
+
+import { useState,useEffect } from "react";
+
+
+
+
+let ThirdVp = ()=>{
+    const {id} = useParams();
+
+    const [companyList,setCompanyList] = useState([]);
+
+
+
+
+
+
+
+
+    //
+    let loadData=async()=>{
+        let array = await axios.get("https://kristhomas.onrender.com/admin/get");
+        let onPageProduct = await axios.get(`https://kristhomas.onrender.com/user/${id}`);
+        let company = onPageProduct.data.foundVehicle.company;
+
+        let unFilteredArray = array.data.vehicles;
+
+        let filteredArray = unFilteredArray.filter((vehicle)=>{
+            return vehicle.company === company;
+        
+        })
+        let superFilteredArray = filteredArray.filter((vehicle)=>{
+            return vehicle._id !== onPageProduct.data.foundVehicle._id;
+        })
+        // for(let vehicle in unFilteredArray){
+        //     console.log("Company :",unFilteredArray[vehicle].company);
+        // }
+        console.log("ON page product id :" ,onPageProduct.data.foundVehicle._id);
+        console.log("Super Filtered Array",superFilteredArray);
+        // console.log("Onpage product " , onPageProduct.data.foundVehicle.company);
+
+        setCompanyList(superFilteredArray);
+
+
+
+
+    }
+    
+    useEffect(()=>{
+        loadData();
+    },[]);
+
+
+    return <div className="ThirdVp">
+        <h3>By same manufacturer</h3>
+        <hr style={{color:"#ee1415"}}/>
+        <div className="similar-match-div">
+       {
+        companyList.map(vehicle=>(
+            <Card className="similar-match-card">
+                <Card.Img src={vehicle.imgSrc} id="similar-match-image" fluid></Card.Img>
+                <Card.Body className="similar-match-card-body">
+                    <Card.Title>{vehicle.name}</Card.Title>
+                    <Card.Text id="details-card-price">
+                    ₹.{vehicle.price} - {Number(vehicle.price)+25000}
+                    </Card.Text>
+                    <Link to={`/user/${vehicle._id}`}><Button variant="outline-danger" id="details-card-button">View Product Details</Button></Link>
+      </Card.Body>
+
+
+
+            </Card>
+        ))
+       }
+       </div>
+
+
+    </div>
+}
+
+
+export default ThirdVp;
--- src/components/ViewVehicle/viewVehicle.jsx
+++ src/components/ViewVehicle/viewVehicle.jsx
@@ -2,12 +2,14 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import FirstVP from "./vehicle-1vh/firstvp";
 import SalientFeatures from "./vehicle-2vp/secondvp";
+import ThirdVp  from "./vehicle-3vp/thirdvp";
 
 let ViewVehicle = ()=>{
     return <>
     <Header/>
     <FirstVP/>
     <SalientFeatures/>
+    <ThirdVp/>
 
 
 
