d8eae9b853fa2a755876075b60076eed0f7d97e8
Krislucas <krislucasthomas@gmail.com>
2023-09-09 15:03:15 +0530

    Removed buggy latest feature

--- src/components/Blog page/red-strip/redstrip.jsx
+++ src/components/Blog page/red-strip/redstrip.jsx
@@ -7,23 +7,25 @@ import { useEffect,useState } from "react";
 
 
 let RedStrip= ()=>{
-    const [latest,setLatest] = useState([]);
+    // const [latest,setLatest] = useState({
+    //     mainHeading:''
+    // });
 
 
-    //
-    let loadData = async()=>{
-        let data = await axios.get('https://kristhomas.onrender.com/user/blogs/all/allblogs');
+    // //
+    // let loadData = async()=>{
+    //     let data = await axios.get('https://kristhomas.onrender.com/user/blogs/all/allblogs');
         
-        setLatest(data.data.allBlogs.reverse()[0]);
-        console.log("red strip: ",latest);
-    }
+    //     setLatest(data.data.allBlogs.reverse()[0]);
+    //     console.log("red strip: ",data.data.allBlogs.reverse()[0].mainHeading);
+    // }
 
 
-    useEffect(()=>{
-        loadData();
-        },[]);
+    // useEffect(()=>{
+    //     loadData();
+    //     },[]);
     return <div className="red-strip">
-        <h6> <marquee width="100%" direction="right" height="100px"><span id="boldandbig">Latest: </span><span id="notbig">{latest.mainHeading}</span></marquee></h6>
+        <h6> <marquee width="100%" direction="right" height="100px"><span id="boldandbig"></span><span id="notbig"></span></marquee></h6>
 
 
 
